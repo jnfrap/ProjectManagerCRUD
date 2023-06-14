649fd1be0f7ab56026882007f6ae3c269ee95be4
jnfrap <jnfr97@gmail.com>
2023-06-14 10:29:59 +0200

    No CORS restrictions

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { bddConnectionMiddleware } from "./middleware/bddconnection";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({origin: ["*"]}));
+app.use(cors());
 app.use(express.json());
 app.use(bddConnectionMiddleware);
 app.use(router);
